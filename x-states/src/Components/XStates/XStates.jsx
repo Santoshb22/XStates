e274cce313e9d13a17dff3fd0548449943a92992
Santoshb22 <sb289848@gmail.com>
2024-09-28 23:23:21 +0530

    test-case

--- x-states/src/Components/XStates/XStates.jsx
+++ x-states/src/Components/XStates/XStates.jsx
@@ -11,12 +11,11 @@ const XStates = () => {
   const [selectedState, setSelectedState] = useState('');
   const [selectedCity, setSelectedCity] = useState('');
 
-  const baseUrl = 'https://crio-location-selector.onrender.com/';
 
   useEffect(() => {
     const fetchCountries = async () => {
       try {
-        const res = await fetch(`${baseUrl}countries`);
+        const res = await fetch('https://crio-location-selector.onrender.com/countries');
         const data = await res.json();
         if (!res.ok) throw new Error('Network response was not ok');
         setCountries(data);
@@ -33,7 +32,7 @@ const XStates = () => {
 
     const fetchStates = async () => {
       try {
-        const res = await fetch(`${baseUrl}country=${selectedCountry}/states`);
+        const res = await fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`);
         const data = await res.json();
         setStates(data);
         setSelectedState(''); 
@@ -52,7 +51,7 @@ const XStates = () => {
     const fetchCities = async () => {
       try {
         const res = await fetch(
-          `${baseUrl}country=${selectedCountry}/state=${selectedState}/cities`
+         `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`
         );
         const data = await res.json();
         setCities(data);
@@ -64,32 +63,49 @@ const XStates = () => {
     fetchCities();
   }, [selectedState]);
 
+
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
+  }
+
+  const handleStateChange = (event) => {
+    setSelectedState(event.target.value);
+  }
+
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+    setStates([]);
+    setCities([]);
+  }
+
   return (
     <div className="xStates">
       <h1>Select Location</h1>
       <div className="dropDowns">
         <div className="countries">
             <DropDown data={countries} 
-            onChange={(e) =>
-               {setSelectedCountry(e.target.value) 
-                setSelectedState('')
-               setSelectedCity('')}}
-         dropdownType="Country" />
+            onChange={handleCountryChange}
+            dropdownType="Country" />
         </div>
 
         <div className="states">
-            <DropDown data={states} onChange={(e) => setSelectedState(e.target.value)} dropdownType="State" isDisabled={!selectedCountry}/>
+            <DropDown data={states} 
+            onChange={handleStateChange} 
+             dropdownType="State" isDisabled={!selectedCountry}/>
         </div>
 
         <div className="cities">
-            <DropDown data={cities} onChange={(e) => setSelectedCity(e.target.value)} dropdownType="City" isDisabled={!selectedState}/>
+            <DropDown data={cities} onChange={handleCityChange} dropdownType="City" isDisabled={!selectedState}/>
         </div>
       </div>
 
       {selectedCity && selectedCountry && selectedState && (
         <div className="showAddress">
           <p>
-            You selected <span className="country">{selectedCountry}</span>,{' '} {selectedState}, {selectedCity}
+            You selected <span className="country">{selectedCountry}</span>,{' '}
+            <span className="cityState">
+              {selectedState}, {selectedCity}
+            </span>
           </p>
         </div>
       )}
